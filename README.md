aa0aba5d5c3dc008921d8238aba5c97b3c86346c
Egilson Cabral <egilsoncabral@hotmail.com>
2019-11-24 13:50:09 -0300

    increased perfomance

--- README.md
+++ README.md
@@ -1,1 +1,80 @@
-# loadermongo
+# Loader Mongo Data
+This is a personal project, whose purpose is load some datas to give support to the [TB_Challenge](https://github.com/egilsoncabral/tb_challenge) project.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+* JDK 8
+* [Maven](https://maven.apache.org/)
+
+## Build/Usage
+
+	1. Clone project
+
+		$ git clone https://github.com/egilsoncabral/tb_challenge.git
+		
+	2. Build the project
+
+	    - For build the project, you will need to execute the instructions
+        	
+         	$ mvn clean package
+         	$ docker-compose up
+        	
+        - Now, clone the data loader project, to load the datas on Mongo database:
+        	
+        	$ git clone https://github.com/egilsoncabral/loadermongo.git
+        
+        - Enter in the project folder and execute:	
+        
+            $ mvn clean package
+            
+        - Go to /target folder, take the .jar file generated and put it in a folder of your own.
+        
+        - Take the dataset file in (https://data.gov.ie/dataset/dublin-bus-gps-sample-data-from-dublin-city-council-insight-project), download one extract, and from that extract, use 1 example CSV as input. Put it in the same folder that you put the jar file.
+        
+        - Execute:
+            $ mvn springboot:run -Dspring-boot.run.arguments=--input.file.name={$filename},--host.address={host}
+	
+	    P.S: $filename example: bus.csv, for the host use "locahost" for docker CE or "192.168.99.100" for docker toolbox
+        
+        - Now access the url:
+            > http://192.168.99.100:8080/swagger-ui.html (If you are using docker toolbox)
+            > http://localhost:8080/swagger-ui.html (If you are using docker CE)    
+    
+### Exposed four endpoints:
+
+```
+/operators
+```
+
+```
+/vehicles
+```
+
+```
+/vehicleAtStop
+```
+
+```
+/vehicleTrace
+```
+
+#### Used Technologies
+
+* Java 8
+* Lombok for data models
+* SpringBoot 
+* Docker
+* Swagger
+* MongoDB
+
+## Versioning
+
+For versioning, i used the gitHub platform. For more details, (https://github.com/)
+
+## Authors
+
+* **Egilson Cabral** - [TBChallenge](https://github.com/egilsoncabral/tb_challenge)
--- src/main/java/com/challenge/batch/DataProcessJob.java
+++ src/main/java/com/challenge/batch/DataProcessJob.java
@@ -11,6 +11,9 @@ import org.springframework.batch.core.configuration.annotation.DefaultBatchConfi
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.job.flow.support.SimpleFlow;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.data.MongoItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
@@ -24,11 +27,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableBatchProcessing
@@ -44,8 +50,8 @@ public class DataProcessJob extends DefaultBatchConfigurer {
     @Autowired
     private MongoTemplate mongoTemplate;
 
-    @Value("${input.file.name}")
-    private static String fileName;
+    @Value("file:${input.file.name}")
+    private Resource resourceFile;
 
 
     @Bean
@@ -56,7 +62,7 @@ public class DataProcessJob extends DefaultBatchConfigurer {
     @Bean
     public TaskExecutor taskExecutor() {
         SimpleAsyncTaskExecutor t = new SimpleAsyncTaskExecutor();
-        t.setConcurrencyLimit(10);
+        t.setConcurrencyLimit(90);
         return t;
     }
 
@@ -68,27 +74,113 @@ public class DataProcessJob extends DefaultBatchConfigurer {
 
     @Bean
     public Job readCSVFile() {
-        return jobBuilderFactory.get("readCSVFile")
-                .incrementer(new RunIdIncrementer())
-                .listener(jobExecutionListener())
-                .flow(step()).end().build();
+        Job job = null;
+        if (resourceFile != null && resourceFile.exists()){
+            job = jobBuilderFactory.get("readCSVFile")
+                    .incrementer(new RunIdIncrementer())
+                    .start(splitFlow())
+                    .build()
+                    .build();
+        } else {
+            log.error("File not found!");
+            log.error("Please, inform the file name as params such as -Dinput.file.name=xxxx.csv.");
+        }
+        return job;
+    }
+
+
+    @Bean
+    public Flow splitFlow() {
+        return new FlowBuilder<SimpleFlow>("splitFlow")
+                .split(taskExecutor())
+                .add(flow1(), flow2(), flow3(), flow4(), flow5(), flow6(), flow7(), flow8(), flow9(), flow10())
+                .build();
+    }
+
+    @Bean
+    public Flow flow1() {
+        return new FlowBuilder<SimpleFlow>("flow1")
+                .start(step())
+                .build();
+    }
+
+    @Bean
+    public Flow flow2() {
+        return new FlowBuilder<SimpleFlow>("flow2")
+                .start(step())
+                .build();
+    }
+
+    @Bean
+    public Flow flow3() {
+        return new FlowBuilder<SimpleFlow>("flow3")
+                .start(step())
+                .build();
+    }
+
+    @Bean
+    public Flow flow4() {
+        return new FlowBuilder<SimpleFlow>("flow4")
+                .start(step())
+                .build();
+    }
+
+    @Bean
+    public Flow flow5() {
+        return new FlowBuilder<SimpleFlow>("flow5")
+                .start(step())
+                .build();
+    }
+
+    @Bean
+    public Flow flow6() {
+        return new FlowBuilder<SimpleFlow>("flow6")
+                .start(step())
+                .build();
+    }
+
+    @Bean
+    public Flow flow7() {
+        return new FlowBuilder<SimpleFlow>("flow7")
+                .start(step())
+                .build();
+    }
+
+    @Bean
+    public Flow flow8() {
+        return new FlowBuilder<SimpleFlow>("flow8")
+                .start(step())
+                .build();
+    }
+
+    @Bean
+    public Flow flow9() {
+        return new FlowBuilder<SimpleFlow>("flow9")
+                .start(step())
+                .build();
+    }
+
+    @Bean
+    public Flow flow10() {
+        return new FlowBuilder<SimpleFlow>("flow10")
+                .start(step())
+                .build();
     }
 
     @Bean
     public Step step() {
-        return stepBuilderFactory.get("step").<VehicleDetail, VehicleDetail>chunk(8)
+        return stepBuilderFactory.get("step").<VehicleDetail, VehicleDetail>chunk(10)
                 .reader(reader())
                 .writer(writer())
-                .taskExecutor(taskExecutor())
+                .listener(jobExecutionListener())
                 .listener(stepExecutionListener())
-                .throttleLimit(10).build();
+                .build();
     }
 
     @Bean
-    public FlatFileItemReader<VehicleDetail> reader() {
+    public FlatFileItemReader<VehicleDetail> reader(){
         FlatFileItemReader<VehicleDetail> reader = new FlatFileItemReader<>();
-        File file = new File(fileName);
-        reader.setResource(new ClassPathResource("bus_data.csv"));
+        reader.setResource(resourceFile);
         reader.setLineMapper(new DefaultLineMapper<VehicleDetail>() {{
             setLineTokenizer(new DelimitedLineTokenizer() {{
                 setNames(new String[]{"timestamp", "lineId",
@@ -99,6 +191,7 @@ public class DataProcessJob extends DefaultBatchConfigurer {
                 setTargetType(VehicleDetail.class);
             }});
         }});
+
         return reader;
     }
 
@@ -111,10 +204,6 @@ public class DataProcessJob extends DefaultBatchConfigurer {
     }
 
     public static void main(String[] args) {
-        fileName = args[0];
-        if (fileName == null){
-            log.error("Please, inform the file name.");
-        }
         long time = System.currentTimeMillis();
         SpringApplication.run(DataProcessJob.class, args);
         time = System.currentTimeMillis() - time;
